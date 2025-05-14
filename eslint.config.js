52efc7c886204592578f8d4346b2349d34ac3e9f
Lopesmatheus1 <lopesmatheus1@hotmail.com>
2025-05-14 20:12:25 -0300

    chore: add pre commit git hook setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
-]);
+])
